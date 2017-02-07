9c3b0c68a2eaead527df63d915926f0c2fb2ef17
Matthew Edge-Williams <mat.edge@gmail.com>
2017-02-07 14:22:55 +1100

    added name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207031513) do
+ActiveRecord::Schema.define(version: 20170207032223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20170207031513) do
     t.boolean  "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "name"
   end
 
 end
