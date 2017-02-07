00dda4b1f246a7140832020fe68ef47affa155b7
Matthew Edge-Williams <mat.edge@gmail.com>
2017-02-08 10:11:41 +1100

    user id working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  before_action :check_if_logged_in, :only => [:edit, :update]
+  before_action :check_if_admin, :only => [:index]
   # GET /users
   # GET /users.json
   def index
