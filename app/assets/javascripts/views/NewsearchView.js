a64c1ea0a8b3bd41976c987055b60664ebde75b3
Matthew Edge-Williams <mat.edge@gmail.com>
2017-02-08 16:45:20 +1100

    Searchin results finally work

--- app/assets/javascripts/views/NewsearchView.js
+++ app/assets/javascripts/views/NewsearchView.js
@@ -13,13 +13,22 @@ app.NewsearchView = Backbone.View.extend({
 
     var $origin = $('#origin').val()
     var $destination = $('#destination').val()
-    var view = this
+    var view = this;
     this.collection.fetch().done(function(results){
       var searchResults = view.collection.where({origin:$origin,destination:$destination})
       console.log(searchResults);
+      //search results get the flight id out
+      view.$el.find('table').show();
+      _(searchResults).each(function (p) {
+        var searchList = new app.SearchListView({model: p});
+        searchList.render();
+      });
+
     })
 
   }
+
+
 
 
 
