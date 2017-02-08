fa36d4c9c6183800db230d7c231d2c923b405795
Matthew Edge-Williams <mat.edge@gmail.com>
2017-02-08 17:16:42 +1100

    weee

--- app/assets/javascripts/views/NewsearchView.js
+++ app/assets/javascripts/views/NewsearchView.js
@@ -18,12 +18,14 @@ app.NewsearchView = Backbone.View.extend({
       var searchResults = view.collection.where({origin:$origin,destination:$destination})
       console.log(searchResults);
       //search results get the flight id out
+      //search results get the flight id out
       view.$el.find('table').show();
       _(searchResults).each(function (p) {
         var searchList = new app.SearchListView({model: p});
         searchList.render();
       });
 
+  // new search list view.  loop through like post
     })
 
   }
