f11e1e96a4ea4f77a95041d440fb5fe471854e2a
Mel <meljamei.info@gmail.com>
2017-02-09 14:20:55 +1100

    Lets finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,15 +26,12 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:create] = "Account created successfully"
+      redirect_to root_path
+    else
+      render :new
     end
   end
 
@@ -70,6 +67,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :admin)
+      params.require(:user).permit(:email, :admin, :password, :password_confirmation , :name)
     end
 end
